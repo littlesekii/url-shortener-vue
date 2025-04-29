4ef7163bb584c891ffda6485524c0b8df51b5c02
littlesekii <contact.sekii@gmail.com>
2025-04-29 20:16:24 -0300

    project initialized

---
+++ src/components/UrlFormComponent.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+const props = defineProps(["buttonText"]);
+</script>
+
+<template>
+  <form class="flex f-centered" @submit.prevent="">
+    <input class="input-url" type="url" placeholder="Enter the link here" />
+    <button class="input-button">{{ props.buttonText }}</button>
+  </form>
+</template>
+
+
+<style scoped>
+form {
+  width: 100%;
+}
+
+.input-url {
+  width: 50%;
+  padding: 10px 15px;
+  margin-right: 5px;
+
+  border-radius: 10px;
+  border: none;
+
+  font-size: 12pt;
+}
+
+.input-button {
+  padding: 10px 15px;
+
+  border-radius: 10px;
+  border: none;
+
+  cursor: pointer;
+
+  font-size: 12pt;
+  font-weight: bold;
+
+  background-color: var(--brand-color);
+  color: white;
+}
+</style>
---
+++ src/views/UrlShortenerView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import UrlFormComponent from "@/components/UrlFormComponent.vue";
+
+
+</script>
+
+<template>
+  <main class="container flex f-column">
+    <h1 class="title">
+      Url Shortener
+    </h1>
+
+    <UrlFormComponent button-text="Encurtar" />
+  </main>
+</template>
+
+<style scoped>
+
+.title {
+  text-align: center;
+}
+
+.container {
+  width: 1040px;
+  max-width: 90%;
+  margin: auto;
+}
+</style>
