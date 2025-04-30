b4328578186600983fc89f5a87db23c772aaba6a
littlesekii <contact.sekii@gmail.com>
2025-04-30 13:17:11 -0300

    env.: added vercel speed insights component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { /*RouterLink, */ RouterView } from "vue-router";
-import { Analytics } from '@vercel/analytics/vue';
+import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 
 </script>
 
 <template>
   <RouterView />
   <Analytics />
+  <SpeedInsights />
 </template>
 
 <style scoped>
