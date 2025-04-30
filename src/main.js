0f61801ef45bc7134ce9b50b535535c8037ab0a4
littlesekii <contact.sekii@gmail.com>
2025-04-30 11:25:31 -0300

    feat.: added dynamic favicon based on browser theme

--- src/main.js
+++ src/main.js
@@ -9,3 +9,13 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
+
+//changes logo based on browser theme
+const setFavicon = () => {
+  const favicon = document.querySelector("link[rel='icon']");
+  favicon.href = (window.matchMedia("(prefers-color-scheme: dark)").matches) 
+    ? "/favicon_dark.ico"
+    : "/favicon_light.ico";
+};
+
+setFavicon();
