c596044dc5bfd4230e05c4197a5d9eff4b6c0086
littlesekii <contact.sekii@gmail.com>
2025-04-30 12:55:09 -0300

    env.: fixed vercel analytics 3x

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { /*RouterLink, */ RouterView } from "vue-router";
+import { Analytics } from '@vercel/analytics/vue';
 
 </script>
 
 <template>
   <RouterView />
+  <Analytics />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { inject } from "@vercel/analytics"
-
 const app = createApp(App);
 
 app.use(router);
@@ -21,6 +19,3 @@ const setFavicon = () => {
 };
 
 setFavicon();
-
-
-inject();
