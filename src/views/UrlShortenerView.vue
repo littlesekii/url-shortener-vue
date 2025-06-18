52b79b5ea9c7522610b4dbef2216fa98cd03beb6
littlesekii <contact.sekii@gmail.com>
2025-06-18 18:26:53 -0300

    patch.: reduced description text even more

--- src/views/UrlShortenerView.vue
+++ src/views/UrlShortenerView.vue
@@ -123,7 +123,7 @@ function copyUrl(url) {
           <a target="_blank" href="https://www.x.com">X</a>, 
           <a target="_blank" href="https://www.facebook.com">Facebook</a>, 
           <a target="_blank" href="https://www.linkedin.com">LinkedIn</a>, 
-           blogs and many other sites. When your short link is ready, just copy and share it everywhere! 
+           blogs and many other sites. When your short link is ready, copy and share it everywhere! 
            <!-- After shortening the URL, check how many clicks it received. -->
         </p>
         <h2>Big links no more!</h2>
